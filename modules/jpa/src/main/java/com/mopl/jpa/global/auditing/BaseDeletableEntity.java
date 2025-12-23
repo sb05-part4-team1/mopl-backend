8750f9763bdb51223588c1912977315a7511a25d
BakJiSeok <commicat2@gmail.com>
2025-12-23 16:38:03 +0900

    fix: EntityListeners 및 No Args constructor 추가

--- modules/jpa/src/main/java/com/mopl/jpa/global/auditing/BaseDeletableEntity.java
+++ modules/jpa/src/main/java/com/mopl/jpa/global/auditing/BaseDeletableEntity.java
@@ -2,11 +2,14 @@ package com.mopl.jpa.global.auditing;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.Instant;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @MappedSuperclass
 public abstract class BaseDeletableEntity extends BaseEntity {
 
--- modules/jpa/src/main/java/com/mopl/jpa/global/auditing/BaseEntity.java
+++ modules/jpa/src/main/java/com/mopl/jpa/global/auditing/BaseEntity.java
@@ -1,18 +1,24 @@
 package com.mopl.jpa.global.auditing;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.Instant;
 import java.util.UUID;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
 
     @Id
