8ed1eaa03e7b84669355a6dfcd47293171529a3a
BakJiSeok <commicat2@gmail.com>
2025-12-24 18:24:54 +0900

    feat: Fix parameter order in UserModel creation in UserFacade

--- applications/api/src/main/java/com/mopl/api/interfaces/api/user/UserCreateRequest.java
+++ applications/api/src/main/java/com/mopl/api/interfaces/api/user/UserCreateRequest.java
@@ -11,19 +11,19 @@ import static com.mopl.domain.model.user.UserModel.RAW_PASSWORD_MAX_LENGTH;
 @Schema(
     example = """
         {
-          "name": "test",
           "email": "test@example.com",
+          "name": "test",
           "password": "P@ssw0rd!"
         }
         """
 )
 public record UserCreateRequest(
     @NotBlank
-    @Size(max = NAME_MAX_LENGTH)
-    String name,
-    @NotBlank
     @Size(max = EMAIL_MAX_LENGTH)
     String email,
+    @NotBlank
+    @Size(max = NAME_MAX_LENGTH)
+    String name,
     @NotBlank
     @Size(max = RAW_PASSWORD_MAX_LENGTH)
     String password
