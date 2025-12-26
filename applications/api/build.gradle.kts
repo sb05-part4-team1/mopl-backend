486a1521bd461ef3030a45dbd5451e0b2e36c612
BakJiSeok <commicat2@gmail.com>
2025-12-26 20:09:27 +0900

    feat: add Spring Security and OAuth2 resource server dependencies in build.gradle.kts

--- applications/api/build.gradle.kts
+++ applications/api/build.gradle.kts
@@ -8,4 +8,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:${project.properties["springDocOpenApiVersion"]}")
+
+    // security
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
 }
