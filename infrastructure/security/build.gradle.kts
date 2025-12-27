f76fd4ca5931016e3ff8d7b28d87c1585e6f8618
BakJiSeok <commicat2@gmail.com>
2025-12-27 20:58:45 +0900

    feat: update comments in build.gradle.kts for clarity on project modules

--- infrastructure/security/build.gradle.kts
+++ infrastructure/security/build.gradle.kts
@@ -3,13 +3,11 @@ plugins {
 }
 
 dependencies {
-    // domain
+    // project modules
     implementation(project(":core:domain"))
-
     // spring security
     api("org.springframework.boot:spring-boot-starter-security")
     api("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
-
     // servlet
     compileOnly("jakarta.servlet:jakarta.servlet-api")
 }
