7df8317d38633043bbe93307046764b4a92318a5
BakJiSeok <commicat2@gmail.com>
2025-12-28 14:13:59 +0900

    feat: refactor AdminInitializerTest to use setup method for improved readability and maintainability

--- applications/api/src/test/java/com/mopl/api/config/AdminInitializerTest.java
+++ applications/api/src/test/java/com/mopl/api/config/AdminInitializerTest.java
@@ -5,10 +5,12 @@ import com.mopl.api.interfaces.api.user.UserCreateRequest;
 import com.mopl.api.interfaces.api.user.UserRoleUpdateRequest;
 import com.mopl.domain.exception.user.DuplicateEmailException;
 import com.mopl.domain.model.user.UserModel;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.UUID;
@@ -29,6 +31,17 @@ class AdminInitializerTest {
     private static final String ADMIN_PASSWORD = "admin1234!";
     private static final UUID ADMIN_ID = UUID.randomUUID();
 
+    @Mock
+    private UserFacade userFacade;
+
+    private AdminInitializer adminInitializer;
+
+    @BeforeEach
+    void setUp() {
+        AdminProperties adminProperties = new AdminProperties(true, ADMIN_EMAIL, ADMIN_NAME, ADMIN_PASSWORD);
+        adminInitializer = new AdminInitializer(adminProperties, userFacade);
+    }
+
     @Nested
     @DisplayName("run()")
     class RunTest {
@@ -37,12 +50,6 @@ class AdminInitializerTest {
         @DisplayName("Admin 계정이 없으면 새로 생성하고 ADMIN 역할을 부여한다")
         void whenAdminNotExists_shouldCreateAdminAndUpdateRole() {
             // given
-            UserFacade userFacade = mock(UserFacade.class);
-            AdminProperties adminProperties = new AdminProperties(
-                true, ADMIN_EMAIL, ADMIN_NAME, ADMIN_PASSWORD
-            );
-            AdminInitializer initializer = new AdminInitializer(adminProperties, userFacade);
-
             UserModel createdUser = mock(UserModel.class);
             given(createdUser.getId()).willReturn(ADMIN_ID);
 
@@ -53,7 +60,7 @@ class AdminInitializerTest {
             given(userFacade.updateRoleInternal(any(UserRoleUpdateRequest.class), eq(ADMIN_ID))).willReturn(adminUser);
 
             // when
-            initializer.run(null);
+            adminInitializer.run(null);
 
             // then
             then(userFacade).should().signUp(any(UserCreateRequest.class));
@@ -64,17 +71,11 @@ class AdminInitializerTest {
         @DisplayName("Admin 계정이 이미 존재하면 생성하지 않는다")
         void whenAdminExists_shouldNotCreateAdmin() {
             // given
-            UserFacade userFacade = mock(UserFacade.class);
-            AdminProperties adminProperties = new AdminProperties(
-                true, ADMIN_EMAIL, ADMIN_NAME, ADMIN_PASSWORD
-            );
-            AdminInitializer initializer = new AdminInitializer(adminProperties, userFacade);
-
             given(userFacade.signUp(any(UserCreateRequest.class)))
                 .willThrow(new DuplicateEmailException(ADMIN_EMAIL));
 
             // when
-            initializer.run(null);
+            adminInitializer.run(null);
 
             // then
             then(userFacade).should().signUp(any(UserCreateRequest.class));
@@ -85,17 +86,11 @@ class AdminInitializerTest {
         @DisplayName("예외 발생 시 에러 로그를 남기고 종료한다")
         void whenExceptionOccurs_shouldLogErrorAndContinue() {
             // given
-            UserFacade userFacade = mock(UserFacade.class);
-            AdminProperties adminProperties = new AdminProperties(
-                true, ADMIN_EMAIL, ADMIN_NAME, ADMIN_PASSWORD
-            );
-            AdminInitializer initializer = new AdminInitializer(adminProperties, userFacade);
-
             given(userFacade.signUp(any(UserCreateRequest.class)))
                 .willThrow(new RuntimeException("Unexpected error"));
 
             // when
-            initializer.run(null);
+            adminInitializer.run(null);
 
             // then
             then(userFacade).should().signUp(any(UserCreateRequest.class));
