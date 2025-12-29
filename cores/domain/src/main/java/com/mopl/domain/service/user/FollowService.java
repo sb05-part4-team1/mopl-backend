7af32704b3c91f9a9a8f11e8437f5466ea777a6b
sooyeonz <5196ksy@naver.com>
2025-12-29 17:41:40 +0900

    feat: save할 때, 새 객체를 만들지 않는 코드로 변경

--- cores/domain/src/main/java/com/mopl/domain/service/user/FollowService.java
+++ cores/domain/src/main/java/com/mopl/domain/service/user/FollowService.java
@@ -1,5 +1,6 @@
 package com.mopl.domain.service.user;
 
+import com.mopl.domain.exception.user.SelfFollowException;
 import com.mopl.domain.model.user.FollowModel;
 import com.mopl.domain.repository.user.FollowRepository;
 
@@ -13,12 +14,11 @@ public class FollowService {
     public FollowModel create(FollowModel followModel) {
 
         if (followModel.getFollowerId().equals(followModel.getFolloweeId())) {
-            throw new IllegalArgumentException("자기 자신을 팔로우할 수 없습니다.");
+            throw new SelfFollowException(followModel.getFollowerId());
         }
 
         return followRepository.findByFollowerIdAndFolloweeId(followModel.getFollowerId(),
             followModel.getFolloweeId())
-            .orElseGet(() -> followRepository.save(FollowModel.create(followModel.getFolloweeId(),
-                followModel.getFollowerId())));
+            .orElseGet(() -> followRepository.save(followModel));
     }
 }
