26cc4817bc0f1b5941df5d903f4498a7d20e9254
sooyeonz <5196ksy@naver.com>
2025-12-29 18:05:19 +0900

    style: 린터 적용

--- cores/domain/src/main/java/com/mopl/domain/exception/user/FollowException.java
+++ cores/domain/src/main/java/com/mopl/domain/exception/user/FollowException.java
@@ -6,11 +6,11 @@ import com.mopl.domain.exception.MoplException;
 
 public abstract class FollowException extends MoplException {
 
-	protected FollowException(String message) {
-		super(message);
-	}
+    protected FollowException(String message) {
+        super(message);
+    }
 
-	protected FollowException(String message, Map<String, Object> details) {
-		super(message, details);
-	}
+    protected FollowException(String message, Map<String, Object> details) {
+        super(message, details);
+    }
 }
--- cores/domain/src/main/java/com/mopl/domain/exception/user/SelfFollowException.java
+++ cores/domain/src/main/java/com/mopl/domain/exception/user/SelfFollowException.java
@@ -5,9 +5,9 @@ import java.util.UUID;
 
 public class SelfFollowException extends FollowException {
 
-	public static final String MESSAGE = "자기 자신을 팔로우할 수 없습니다.";
+    public static final String MESSAGE = "자기 자신을 팔로우할 수 없습니다.";
 
-	public SelfFollowException(UUID id) {
-		super(MESSAGE, Map.of("id", id));
-	}
+    public SelfFollowException(UUID id) {
+        super(MESSAGE, Map.of("id", id));
+    }
 }
