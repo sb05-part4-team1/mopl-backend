58d601d82ac87eb3d468665f45a560f7128d4673
sooyeonz <5196ksy@naver.com>
2025-12-29 14:28:24 +0900

    refactor: FollowService에서 @Component 어노테이션 제거, DomainServiceConfig에서 @Bean 등록

--- cores/domain/src/main/java/com/mopl/domain/service/user/FollowService.java
+++ cores/domain/src/main/java/com/mopl/domain/service/user/FollowService.java
@@ -1,27 +1,24 @@
 package com.mopl.domain.service.user;
 
-import org.springframework.stereotype.Component;
-
 import com.mopl.domain.model.user.FollowModel;
 import com.mopl.domain.repository.user.FollowRepository;
 
 import lombok.RequiredArgsConstructor;
 
-@Component
 @RequiredArgsConstructor
 public class FollowService {
 
-    private final FollowRepository followRepository;
+	private final FollowRepository followRepository;
 
-    public FollowModel create(FollowModel followModel) {
+	public FollowModel create(FollowModel followModel) {
 
-        if (followModel.getFollowerId().equals(followModel.getFolloweeId())) {
-            throw new IllegalArgumentException("자기 자신을 팔로우할 수 없습니다.");
-        }
+		if (followModel.getFollowerId().equals(followModel.getFolloweeId())) {
+			throw new IllegalArgumentException("자기 자신을 팔로우할 수 없습니다.");
+		}
 
-        return followRepository.findByFollowerIdAndFolloweeId(followModel.getFollowerId(),
-            followModel.getFolloweeId())
-            .orElseGet(() -> followRepository.save(FollowModel.create(followModel.getFolloweeId(),
-                followModel.getFollowerId())));
-    }
+		return followRepository.findByFollowerIdAndFolloweeId(followModel.getFollowerId(),
+				followModel.getFolloweeId())
+			.orElseGet(() -> followRepository.save(FollowModel.create(followModel.getFolloweeId(),
+				followModel.getFollowerId())));
+	}
 }
