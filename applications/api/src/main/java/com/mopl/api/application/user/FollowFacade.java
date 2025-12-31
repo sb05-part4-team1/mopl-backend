2dd79c100a8fac327fdb0f6d8b0d29a2ed7f44ea
sooyeonz <5196ksy@naver.com>
2025-12-31 17:43:04 +0900

    feat: 팔로우 취소 facade 추가

--- applications/api/src/main/java/com/mopl/api/application/user/FollowFacade.java
+++ applications/api/src/main/java/com/mopl/api/application/user/FollowFacade.java
@@ -3,7 +3,9 @@ package com.mopl.api.application.user;
 import java.util.UUID;
 
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.mopl.domain.exception.user.AccessDeniedException;
 import com.mopl.domain.model.user.FollowModel;
 import com.mopl.domain.service.user.FollowService;
 import com.mopl.domain.service.user.UserService;
@@ -17,12 +19,26 @@ public class FollowFacade {
     private final FollowService followService;
     private final UserService userService;
 
+    @Transactional
     public FollowModel follow(UUID followerId, UUID followeeId) {
         userService.getById(followerId);
         userService.getById(followeeId);
 
         FollowModel followModel = FollowModel.create(followeeId, followerId);
         return followService.create(followModel);
+    }
+
+    @Transactional
+    public void unFollow(UUID userId, UUID followId) {
+        userService.getById(userId);
+
+        FollowModel follow = followService.getById(followId);
+
+        if (!follow.getFollowerId().equals(userId)) {
+            throw new AccessDeniedException(userId, followId);
+        }
+
+        followService.delete(follow);
     }
 
 }
