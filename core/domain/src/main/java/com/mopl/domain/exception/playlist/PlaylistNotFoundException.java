77b78c179bcc1641136354c5c7dadff73a4c8836
jisung403 <jisung494@gmail.com>
2026-01-06 13:13:32 +0900

    feat: playlist 관련 예외처리 추가

---
+++ core/domain/src/main/java/com/mopl/domain/exception/playlist/PlaylistNotFoundException.java
@@ -0,0 +1,14 @@
+package com.mopl.domain.exception.playlist;
+
+import java.util.Map;
+import java.util.UUID;
+
+public class PlaylistNotFoundException extends PlaylistException {
+
+  public PlaylistNotFoundException(UUID playlistId) {
+    super(
+            PlaylistErrorCode.PLAYLIST_NOT_FOUND,
+            Map.of("playlistId", playlistId)
+    );
+  }
+}
