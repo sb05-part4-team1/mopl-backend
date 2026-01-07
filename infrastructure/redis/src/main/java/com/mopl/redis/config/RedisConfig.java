bc6c584c5a593876f7167b284e16e05d1a33811d
BakJiSeok <commicat2@gmail.com>
2026-01-07 18:25:54 +0900

    feat: implement RedisTemplate configuration for caching operations

--- infrastructure/redis/src/main/java/com/mopl/redis/config/RedisConfig.java
+++ infrastructure/redis/src/main/java/com/mopl/redis/config/RedisConfig.java
@@ -2,38 +2,51 @@ package com.mopl.redis.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-import java.time.Duration;
-
-import static com.fasterxml.jackson.annotation.JsonTypeInfo.*;
+import static com.fasterxml.jackson.annotation.JsonTypeInfo.As;
 
 @Configuration
-@EnableCaching
 public class RedisConfig {
 
     @Bean
-    public RedisCacheConfiguration redisCacheConfiguration(ObjectMapper objectMapper) {
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+    public RedisTemplate<String, Object> redisTemplate(
+        RedisConnectionFactory connectionFactory,
+        ObjectMapper objectMapper
+    ) {
+
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+        ObjectMapper redisObjectMapper = createRedisObjectMapper(objectMapper);
+        GenericJackson2JsonRedisSerializer jsonRedisSerializer =
+            new GenericJackson2JsonRedisSerializer(redisObjectMapper);
+
+        template.setKeySerializer(stringRedisSerializer);
+        template.setHashKeySerializer(stringRedisSerializer);
+
+        template.setValueSerializer(jsonRedisSerializer);
+        template.setHashValueSerializer(jsonRedisSerializer);
+
+        return template;
+    }
+
+
+    @SuppressWarnings("deprecation")
+    private ObjectMapper createRedisObjectMapper(ObjectMapper objectMapper) {
         ObjectMapper redisObjectMapper = objectMapper.copy();
         redisObjectMapper.activateDefaultTyping(
-                LaissezFaireSubTypeValidator.instance,
-                ObjectMapper.DefaultTyping.EVERYTHING,
-                As.PROPERTY
+            LaissezFaireSubTypeValidator.instance,
+            ObjectMapper.DefaultTyping.EVERYTHING,
+            As.PROPERTY
         );
-
-        return RedisCacheConfiguration.defaultCacheConfig()
-                .serializeValuesWith(
-                        RedisSerializationContext.SerializationPair.fromSerializer(
-                                new GenericJackson2JsonRedisSerializer(redisObjectMapper)
-                        )
-                )
-                .prefixCacheNameWith("mopl:")
-                .entryTtl(Duration.ofSeconds(600))
-                .disableCachingNullValues();
+        return redisObjectMapper;
     }
 }
