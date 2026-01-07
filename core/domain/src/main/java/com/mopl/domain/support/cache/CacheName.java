60a3decb6bb9c739a9542d92021c18417c182e78
BakJiSeok <commicat2@gmail.com>
2026-01-08 00:01:44 +0900

    feat: add CacheName class for cache key constants

---
+++ core/domain/src/main/java/com/mopl/domain/support/cache/CacheName.java
@@ -0,0 +1,10 @@
+package com.mopl.domain.support.cache;
+
+public final class CacheName {
+
+    public static final String USERS = "users";
+    public static final String USERS_BY_EMAIL = "users-by-email";
+
+    private CacheName() {
+    }
+}
