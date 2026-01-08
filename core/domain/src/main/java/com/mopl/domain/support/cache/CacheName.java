081c890d05f63557a97901db46542ce4442fa66e
BakJiSeok <commicat2@gmail.com>
2026-01-08 15:23:30 +0900

    feat: add private constructor annotation to CacheName for better encapsulation

--- core/domain/src/main/java/com/mopl/domain/support/cache/CacheName.java
+++ core/domain/src/main/java/com/mopl/domain/support/cache/CacheName.java
@@ -1,10 +1,11 @@
 package com.mopl.domain.support.cache;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class CacheName {
 
     public static final String USERS = "users";
     public static final String USERS_BY_EMAIL = "users-by-email";
-
-    private CacheName() {
-    }
 }
