e47bd35ee147ade7e740b12125f2a860d196d0a7
sooyeonz <5196ksy@naver.com>
2026-01-12 15:20:00 +0900

    feat: Notification Swagger 추가

---
+++ applications/api/src/main/java/com/mopl/api/interfaces/api/notification/NotificationApiSpec.java
@@ -0,0 +1,55 @@
+package com.mopl.api.interfaces.api.notification;
+
+import java.util.UUID;
+
+import org.springframework.http.ResponseEntity;
+
+import com.mopl.domain.exception.ErrorResponse;
+import com.mopl.security.userdetails.MoplUserDetails;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "Notification API", description = "알림 API")
+public interface NotificationApiSpec {
+
+    @Operation(summary = "알림 읽음 처리")
+    @ApiResponse(
+        responseCode = "200",
+        description = "성공"
+    )
+    @ApiResponse(
+        responseCode = "204",
+        description = "성공"
+    )
+    @ApiResponse(
+        responseCode = "400",
+        description = "잘못된 요청",
+        content = @Content(
+            mediaType = "application/json",
+            schema = @Schema(implementation = ErrorResponse.class)
+        )
+    )
+    @ApiResponse(
+        responseCode = "401",
+        description = "인증 오류",
+        content = @Content(
+            mediaType = "application/json",
+            schema = @Schema(implementation = ErrorResponse.class)
+        )
+    )
+    @ApiResponse(
+        responseCode = "500",
+        description = "서버 오류",
+        content = @Content(
+            mediaType = "application/json",
+            schema = @Schema(implementation = ErrorResponse.class)
+        )
+    )
+    ResponseEntity<Void> readNotification(@Parameter(hidden = true) MoplUserDetails userDetails,
+        @Parameter(name = "notificationId", required = true) UUID notificationId);
+}
