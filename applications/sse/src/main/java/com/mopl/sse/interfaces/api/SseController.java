f1ea7f56ea3a3da68cda773eeb1fb3a132b8eb1d
Kim Sooyeon <5196ksy@naver.com>
2026-01-13 10:36:44 +0900

    [feat] SSE 모듈 추가 및 구독 구현, 팔로우 시 알림 발생 코드 추가 (#55)
    * feat: sseFacade 구현
    
    * feat: SseEmitter를 사용하여 repository 구현
    
    * feat: sse 연결 service 구현
    
    * feat: sse Swagger API 추가
    
    * feat: sse 구독 엔드포인트 추가
    
    * build: applications:sse 모듈 추가
    
    * build: sse gradle 셋팅
    
    * build: api에 sse 의존성 추가
    
    * build: sse 모듈 스캔 가능하도록 추가
    
    * feat: NotificationModel 구현
    
    * feat: NotificationEntityMapper 구현
    
    * feat: NotificationResponse, Mapper 구현
    
    * feat: 팔로우 요청시에 알림 발생 코드 추가
    
    * fix: remove unnecessary JPA dependency from build configuration
    
    ---------
    
    Co-authored-by: BakJiSeok <commicat2@gmail.com>

---
+++ applications/sse/src/main/java/com/mopl/sse/interfaces/api/SseController.java
@@ -0,0 +1,32 @@
+package com.mopl.sse.interfaces.api;
+
+import java.util.UUID;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import com.mopl.security.userdetails.MoplUserDetails;
+import com.mopl.sse.application.SseFacade;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/sse")
+@RequiredArgsConstructor
+public class SseController implements SseApiSpec {
+
+    private final SseFacade sseFacade;
+
+    @Override
+    @GetMapping(produces = "text/event-stream")
+    public SseEmitter subscribe(
+        @AuthenticationPrincipal MoplUserDetails userDetails,
+        @RequestParam(required = false) UUID lastEventId
+    ) {
+        return sseFacade.subscribe(userDetails.userId(), lastEventId);
+    }
+}
