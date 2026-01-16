5d01dfd1f9edea86be2aec9dc9aab8ee64cf1a8c
BakJiSeok <commicat2@gmail.com>
2026-01-17 01:39:01 +0900

    refactor: remove unnecessary blank line in JpaConfig

--- infrastructure/jpa/src/main/java/com/mopl/jpa/config/JpaConfig.java
+++ infrastructure/jpa/src/main/java/com/mopl/jpa/config/JpaConfig.java
@@ -10,5 +10,4 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories(basePackages = "com.mopl.jpa.repository")
 @EntityScan(basePackages = "com.mopl.jpa.entity")
 public class JpaConfig {
-
 }
