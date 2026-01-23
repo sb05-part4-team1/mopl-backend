d942aa27871876e874114110c851a6dbcec6d85b
BakJiSeok <commicat2@gmail.com>
2026-01-23 17:41:02 +0900

    refactor: update build.gradle.kts and storage.yaml for AWS S3 integration

--- infrastructure/storage/build.gradle.kts
+++ infrastructure/storage/build.gradle.kts
@@ -3,6 +3,11 @@ plugins {
 }
 
 dependencies {
+    // spring boot
+    implementation("org.springframework.boot:spring-boot-starter")
     // validation
     implementation("org.springframework.boot:spring-boot-starter-validation")
+    // aws s3
+    implementation(platform("software.amazon.awssdk:bom:${property("awsS3SdkVersion")}"))
+    implementation("software.amazon.awssdk:s3")
 }
