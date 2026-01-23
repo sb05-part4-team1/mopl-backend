04238bb7310429d360ebfdd162a3aef11b4484a0
BakJiSeok <commicat2@gmail.com>
2026-01-23 19:53:34 +0900

    refactor: introduce StorageAutoConfig for dynamic storage provider configuration

---
+++ infrastructure/storage/src/main/java/com/mopl/storage/config/StorageAutoConfig.java
@@ -0,0 +1,66 @@
+package com.mopl.storage.config;
+
+import com.mopl.storage.provider.StorageProvider;
+import com.mopl.storage.provider.LocalStorageProvider;
+import com.mopl.storage.provider.S3StorageProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+
+@AutoConfiguration
+@EnableConfigurationProperties(StorageProperties.class)
+@RequiredArgsConstructor
+public class StorageAutoConfig {
+
+    private final StorageProperties storageProperties;
+
+    @Bean
+    @ConditionalOnProperty(name = "mopl.storage.type", havingValue = "local", matchIfMissing = true)
+    public StorageProvider localStorageProvider() {
+        return new LocalStorageProvider(storageProperties.local());
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = "mopl.storage.type", havingValue = "s3")
+    public S3Client s3Client() {
+        StorageProperties.S3 s3 = storageProperties.s3();
+        return S3Client.builder()
+            .region(Region.of(s3.region()))
+            .credentialsProvider(StaticCredentialsProvider.create(
+                AwsBasicCredentials.create(s3.accessKey(), s3.secretKey())
+            ))
+            .build();
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = "mopl.storage.type", havingValue = "s3")
+    public S3Presigner s3Presigner() {
+        StorageProperties.S3 s3 = storageProperties.s3();
+        return S3Presigner.builder()
+            .region(Region.of(s3.region()))
+            .credentialsProvider(StaticCredentialsProvider.create(
+                AwsBasicCredentials.create(s3.accessKey(), s3.secretKey())
+            ))
+            .build();
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = "mopl.storage.type", havingValue = "s3")
+    public StorageProvider s3StorageProvider(
+        S3Client s3Client,
+        S3Presigner s3Presigner
+    ) {
+        return new S3StorageProvider(
+            storageProperties.s3(),
+            s3Client,
+            s3Presigner
+        );
+    }
+}
