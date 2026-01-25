a1b3f74cad9675d94e67a489bc4626bc4aab09bb
BakJiSeok <commicat2@gmail.com>
2026-01-25 23:40:52 +0900

    feat: add batch startup configuration and runner for scheduled jobs

---
+++ applications/batch/src/main/java/com/mopl/batch/config/BatchStartupProperties.java
@@ -0,0 +1,24 @@
+package com.mopl.batch.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Component
+@ConfigurationProperties(prefix = "mopl.batch")
+public class BatchStartupProperties {
+
+    private RunOnStartup runOnStartup = new RunOnStartup();
+
+    @Getter
+    @Setter
+    public static class RunOnStartup {
+        private boolean enabled = false;
+        private List<String> jobs = List.of();
+    }
+}
