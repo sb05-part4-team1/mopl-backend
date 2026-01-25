b4b65abd47bc100eb80822199c2ef06e12f0c604
BakJiSeok <commicat2@gmail.com>
2026-01-26 03:55:47 +0900

    refactor: remove unused import in JpaReviewRepository

--- infrastructure/jpa/src/main/java/com/mopl/jpa/repository/review/JpaReviewRepository.java
+++ infrastructure/jpa/src/main/java/com/mopl/jpa/repository/review/JpaReviewRepository.java
@@ -8,7 +8,6 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.time.Instant;
-import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
