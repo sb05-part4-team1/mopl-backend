99007a267a3396aa00215756505e224e9a10d43d
BakJiSeok <commicat2@gmail.com>
2026-01-27 03:46:00 +0900

    refactor: implement caching for presigned URLs in S3StorageProvider

--- infrastructure/storage/build.gradle.kts
+++ infrastructure/storage/build.gradle.kts
@@ -5,8 +5,9 @@ plugins {
 dependencies {
     // project modules
     implementation(project(":core:domain"))
-    // validation
+    // spring
     implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework:spring-context")
     // aws s3
     implementation(platform("software.amazon.awssdk:bom:${property("awsS3SdkVersion")}"))
     implementation("software.amazon.awssdk:s3")
