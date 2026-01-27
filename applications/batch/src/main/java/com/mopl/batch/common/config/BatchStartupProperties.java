6e4bbae4422e15bae4f474bab3c094a2b86badb5
BakJiSeok <commicat2@gmail.com>
2026-01-27 20:37:30 +0900

    refactor: update batch startup properties and scheduler configuration for improved functionality

--- applications/batch/src/main/java/com/mopl/batch/common/config/BatchStartupProperties.java
+++ applications/batch/src/main/java/com/mopl/batch/common/config/BatchStartupProperties.java
@@ -3,6 +3,7 @@ package com.mopl.batch.common.config;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 import java.util.List;
 
