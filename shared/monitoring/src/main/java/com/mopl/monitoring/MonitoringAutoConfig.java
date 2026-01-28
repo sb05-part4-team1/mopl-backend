363dc5fbe3c7f6a16692a3e27a0159421d228b28
BakJiSeok <commicat2@gmail.com>
2026-01-28 18:44:11 +0900

    refactor: rename MonitoringAutoConfiguration to MonitoringAutoConfig

--- shared/monitoring/src/main/java/com/mopl/monitoring/MonitoringAutoConfig.java
+++ shared/monitoring/src/main/java/com/mopl/monitoring/MonitoringAutoConfig.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
 @AutoConfiguration
-public class MonitoringAutoConfiguration {
+public class MonitoringAutoConfig {
 
     @Bean
     public MeterRegistryCustomizer<MeterRegistry> commonTagsCustomizer(
