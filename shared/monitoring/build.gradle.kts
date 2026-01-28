a1bf6023e48628e5f6cc101e8236a19060f3c678
BakJiSeok <commicat2@gmail.com>
2026-01-28 16:14:25 +0900

    refactor: enhance monitoring setup with distributed tracing and OpenSearch integration

--- shared/monitoring/build.gradle.kts
+++ shared/monitoring/build.gradle.kts
@@ -3,6 +3,10 @@ plugins {
 }
 
 dependencies {
+    // actuator & prometheus
     api("org.springframework.boot:spring-boot-starter-actuator")
     api("io.micrometer:micrometer-registry-prometheus")
+    // distributed tracing
+    api("io.micrometer:micrometer-tracing-bridge-brave:${project.properties["micrometerTracingVersion"]}")
+    api("io.zipkin.reporter2:zipkin-reporter-brave:${project.properties["zipkinReporterVersion"]}")
 }
