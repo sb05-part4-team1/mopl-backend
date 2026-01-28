f4f5150e63c2c5fdb3ecdc8c1db9e3a7d200a4e2
BakJiSeok <commicat2@gmail.com>
2026-01-28 16:34:33 +0900

    refactor: remove OpenSearch appender from logging configuration

--- shared/logging/build.gradle.kts
+++ shared/logging/build.gradle.kts
@@ -8,8 +8,6 @@ dependencies {
     api("net.logstash.logback:logstash-logback-encoder:${project.properties["logstashLogbackEncoderVersion"]}")
     // slack appender
     api("com.github.maricn:logback-slack-appender:${project.properties["slackAppenderVersion"]}")
-    // opensearch appender (for log aggregation)
-    api("com.internetitem:logback-elasticsearch-appender:1.6")
     // spring (optional - for MdcLoggingFilter)
     compileOnly("org.springframework.boot:spring-boot-starter-web")
     compileOnly("org.springframework.boot:spring-boot-autoconfigure")
